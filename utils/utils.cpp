8049c5dbec3b4c9b991994a8d0118b3219e33fae
Jonas <huegel.jonas@gmail.com>
2023-06-23 13:05:24 -0400

    updated summary helper functions

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -260,6 +260,8 @@ namespace tspm {
             localCounts[omp_get_thread_num()].emplace_back(std::make_pair(sequences[i].seqID,numOfSequences));
         }
 
+        sequences.clear();
+        sequences.shrink_to_fit();
         std::vector<std::pair<std::int64_t, size_t>> sequenceCounts;
         for (size_t i =0 ;i < numOfThreads; ++i) {
             std::vector<std::pair<std::int64_t, size_t>> counts = localCounts[i];
@@ -299,7 +301,8 @@ namespace tspm {
                 localCounts[omp_get_thread_num()].clear();
             }
         }
-
+        sequences.clear();
+        sequences.shrink_to_fit();
         for (size_t i =0 ;i < numOfThreads; ++i) {
             std::vector<std::pair<std::int64_t, size_t>> counts = localCounts[i];
             if(counts.empty()){
